74e3ca25f26b55e748e39101cbbbc8f8fe06d86b
Adrian Waite <ajwaite06@gmail.com>
2019-10-13 20:50:29 -0700

    a

--- src/sketch.js
+++ src/sketch.js
@@ -1,25 +1,44 @@
+var framerate = 60; // Amount of refreshes per second. Standard is 60.
 function setup() {
+  frameRate(framerate);
   createCanvas(windowWidth, windowHeight); // Feel free to change these values.
 }
+/* PLAY WITH THESE VALUES */
 var jumpAmount = 20; // how many jumps you can perform midair
-var jumpCombo = 0; // don't touch this
-var mvLeft = false; // don't touch this
-var mvRight = false; // don't touch this
+var jumpCombo = 0; // How many jumps the player has made since touching the ground
+var mvLeft = false; // Is the player moving left?
+var mvRight = false; // Is the player moving right?
 var moveSpeed = 5; // how fast you move when pressing right or left
 var x = 100; // pixels between the left side of the box and screen. See y.
 var y = 100; // how many pixels from the top the box starts. Note this number is the top of the box.
-var fallspeed = 0; // don't touch this
+var fallspeed = 0; // how fast the player is falling
 var gravity = 9.8; // Simple limiter on the down speed of the cube.
-var accel = 1; // don't suggest increasing this above 1.
+var accel = 1; // don't suggest increasing this above 1. This number is added to fallspeed every frame.
 var jumpPower = 20; // the force of the jump, in pixels.
 var doubleJump = false; // Whether or not the box can jump midair.
 function draw() {
   background(220);
   rect(x, y, 100, 100);
-  text("Falling Speed: " + -fallspeed, 50, 50);
+  if (fallspeed === 0) {
+    text("Falling/Upward Speed: " + fallspeed, 50, 50);
+  } else if (fallspeed > 0) {
+    text("Falling Speed: " + fallspeed, 50, 50);
+  } else if (fallspeed < 0) {
+    text("Upward Speed: " + -fallspeed, 50, 50);
+  } else {
+    text("Speed broke. Value:" + fallspeed, 50, 50);
+  }
   text("Y: " + y, 50, 60);
   text("X: " + x, 50, 70);
-  text("Jump Combo: " + jumpCombo, 50, 80);
+  if (jumpCombo >= jumpAmount) {
+    text("Jump Combo: " + jumpCombo + "!", 50, 80);
+  } else if (jumpCombo < 0) {
+    text("Jump Combo: " + jumpCombo + "?", 50, 80);
+  } else {
+    text("Jump Combo: " + jumpCombo, 50, 80);
+  }
+  text(width, 50, 100);
+  text("FPS: " + framerate, 50, 90);
   if (y <= height - 102) {
     fallspeed += accel;
     y += fallspeed;
